60abc3a76fa6db9943e94265e1b687eba3d0d112
Janis Schneeberger <janis.schneeberger@outlook.com>
2021-04-01 17:56:36 +0200

    Currently creates an html document with a circle at every components location and the corresponding id

--- kicad-schematic-parser.js
+++ kicad-schematic-parser.js
@@ -3,16 +3,21 @@
 const fs = require('fs');
 
 const reader = require('./lib/read_schematic');
+const libReader = require('./lib/read_lib');
 const writer = require('./lib/write_schematic');
+const htmlWriter = require('./lib/create_html');
 
 const { inspect } = require('./lib/util');
 
 const { set_defaults } = require('./lib/set_defaults');
 const { generate_footprint } = require('./lib/generate_fields');
 
-// The filename is the first and only argument to the script.
+// Read schematic and Lib file
+// Schematic file path needs to be passed as the first argument of the script, Lib file path as the second
 const schematic = reader(
     fs.readFileSync(process.argv[2], 'utf-8'));
+const lib = libReader(
+    fs.readFileSync(process.argv[3], 'utf-8'));
 
 // Loop over all components and set default fields and values:
 schematic.Comp.forEach(set_defaults);
@@ -21,7 +26,11 @@ schematic.Comp.forEach(set_defaults);
 schematic.Comp.forEach(generate_footprint);
 
 // Dump the whole thing to stdout:
-console.log(writer(schematic));
+// console.log(writer(schematic));
+
+
+// Create html document displaying the kicad schematic
+htmlWriter(schematic);
 
 // inspect(schematic.Comp);
 // inspect(default_fields);
