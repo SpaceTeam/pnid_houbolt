b5e61667a3b2d2f7e1f20add56226c9c66652e35
LuPro <luis.immanuel@gmx.at>
2021-07-04 22:37:29 +0200

    implement first version of config eval parser Can parse a config.json file (currently still hardcoded in the .js) to find out behavior for each kind of node. evals in config have arrays "inVars" and "outVars" to use and return data. inVars has element "value", outVars has recognized elements "color" and "text".

--- client/js/changeValues.js
+++ client/js/changeValues.js
@@ -1,4 +1,22 @@
-document.getElementById("OV_FP_FT1").setAttribute("stroke", "green");
+var configString = `{
+  "solenoid": {
+    "states": [
+      "purge_solenoid",
+      "fuel_pressurize_solenoid",
+      "ox_pressurize_solenoid",
+      "fuel_depressurize_solenoid"
+    ],
+    "eval": "inVars['value'] > 10 ? outVars['color']='open' : outVars['color']='closed'"
+  },
+  "customSolenoid": {
+    "states": [
+      "fuel_pressurize_solenoid"
+    ],
+    "eval": "if (inVars['value'] > 10) { outVars['color']='undefined'; outVars['value']='open'+inVars['value'] } else { outVars['color']='undefined'; outVars['value']='open'+inVars['value']}"
+  }
+}`;
+
+var config = JSON.parse(configString);
 
 function startLoop() {
 	var i;
@@ -22,19 +40,33 @@ function startLoop() {
 	
 }
 
-var testData = [{"name": "solenoid1", "value": 12.0}, {"name": "solenoid2", "value": 8.0}];
+async function runTests()
+{
+	var testData = [{"name": "purge_solenoid", "value": 12.0}];
+	updatePNID(testData);
+	await sleep(1000);
+	var testData = [{"name": "purge_solenoid", "value": 6.0}, {"name": "fuel_depressurize_solenoid", "value": 12.0}];
+	updatePNID(testData);
+	await sleep(1000);
+	var testData = [{"name": "fuel_pressurize_solenoid", "value": 20.0}];
+	updatePNID(testData);
+	await sleep(1000);
+	var testData = [{"name": "fuel_pressurize_solenoid", "value": 5.0}, {"name": "fuel_depressurize_solenoid", "value": 1.0}];
+	updatePNID(testData);
+	await sleep(1000);
+}
 
-//updatePNID([{"name": "solenoid1", "value": 12.0}, {"name": "solenoid2", "value": 8.0}]);
+//updatePNID(testData);
 //updatePNID([{"name": "PnID-Valve_Solenoid", "value": 12.0}, {"name": "solenoid2", "value": 8.0}]);
 function updatePNID(stateList)
 {
-	console.log(stateList);
+	console.log("Updating PnID with:", stateList);
 	
 	for (stateIndex in stateList)
 	{
 		let stateName = stateList[stateIndex]["name"];
 		let stateValue = stateList[stateIndex]["value"];
-		console.log("out name: '", stateName, "' value:",  stateValue);
+		//console.log("out name: '", stateName, "' value:",  stateValue);
 		setState(stateList[stateIndex]);
 	}
 	
@@ -43,11 +75,58 @@ function updatePNID(stateList)
 
 function setState(state)
 {
-	$(document).find("g." + state["name"]).find("text.value").text(state["value"]);
+	let elementGroup = $(document).find("g." + state["name"]);
+	elementGroup.find("text.value").text(state["value"]);
+	console.log("Found following elements to update:", $(document).find("g." + state["name"]));
 	
-	$.getJSON("client/js/config.json", function (config) {
-		console.log(config["solenoid"]);
-	});
+	//In Variables for the eval() code specified in config.json. Will be reset/overwritten for every state and every loop
+	let inVars = {
+		"value" : state["value"]
+	};
+	
+	//State storage for the eval() code specified in config.json //TBD (let eval code create entries? pre-define generic name entries?)
+	var stateVars = { };
+	
+	//Return values from eval() code specified in config.json. Will be applied to PnID and cleared for every state and every loop
+	let outVars = { };
+	
+	//traverse JSON to find all evals applicable to current element. evals later in JSON overwrite changes made by evals earlier (if they change the same things)
+	let configProperties = Object.keys(config);
+	for (propIndex in configProperties)
+	{
+		//console.log("searching for state", state["name"], "from available states:", config[configProperties[propIndex]]["states"]);
+		if (config[configProperties[propIndex]]["states"].includes(state["name"])) //if the currently traversed property contains our state, check for eval
+		{
+			console.log("Found following code to run for element:", config[configProperties[propIndex]]["eval"]);
+			eval(config[configProperties[propIndex]]["eval"]);
+		}
+	}
+	
+	//fetch all attributes of the element group
+	let attributes = elementGroup.prop("attributes");
+	//console.log("Found these attributes:", attributes);
+	
+	//apply all outVars to PnID
+	if ("color" in outVars)
+	{
+		for (attrIndex in attributes)
+		{
+			if (attrIndex == "length") //otherwise JS also iterates over control elements in the array for whatever stupid reason
+			{
+				break;
+			}
+			let attribute = attributes[attrIndex];
+			let re = /data-pnid-.*/;
+			if (re.test(attribute.name))
+			{
+				elementGroup.attr(attribute.name, outVars["color"]);
+			}
+		}
+	}
+	if ("value" in outVars)
+	{
+		elementGroup.find("text.value").text(outVars["value"]);
+	}
 }
 
 function setConfig()
@@ -62,5 +141,5 @@ function saveConfig()
 
 function sleep (time)
 {
-  return new Promise((resolve) => setTimeout(resolve, time));
+	return new Promise((resolve) => setTimeout(resolve, time));
 }
