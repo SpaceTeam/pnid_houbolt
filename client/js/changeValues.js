0999f1eb59b0657793c811a9822c4cf993d75505
LuPro <luis.immanuel@gmx.at>
2021-07-05 23:06:41 +0200

    minor code restructure, split config, initial wire update support moved apply functionality from setState into another function split config into default config and custom config files (still hardcoded in js) added wire colors in .css and outVars['wireUpdate'] support

--- client/js/changeValues.js
+++ client/js/changeValues.js
@@ -1,21 +1,23 @@
 var configString = `{
   "solenoid": {
     "states": [
-      "purge_solenoid",
-      "fuel_pressurize_solenoid",
-      "ox_pressurize_solenoid",
-      "fuel_depressurize_solenoid"
-    ],
-    "eval": "inVars['value'] > 10 ? outVars['color']='open' : outVars['color']='closed'"
-  },
-  "customSolenoid": {
-    "states": [
       "fuel_pressurize_solenoid"
     ],
     "eval": "if (inVars['value'] > 10) { outVars['color']='undefined'; outVars['value']='open'+inVars['value'] } else { outVars['color']='undefined'; outVars['value']='open'+inVars['value']}"
   }
 }`;
 
+//todo: evaluate if default configs may benefit from having a state *blacklist* instead of a state *whitelist* like in the custom configs
+const defaultConfigString = `{
+  "PnID-Valve_Solenoid": {
+    "eval": "inVars['value'] > 10 ? outVars['color']='open' : outVars['color']='closed'"
+  },
+  "PnID-Sensor_Pressure": {
+    "eval": "inVars['value'] > 1 ? outVars['color']='high' : outVars['color']='low'"
+  }
+}`;
+
+const defaultConfig = JSON.parse(defaultConfigString);
 var config = JSON.parse(configString);
 
 function startLoop() {
@@ -51,6 +53,15 @@ async function runTests()
 	var testData = [{"name": "fuel_pressurize_solenoid", "value": 20.0}];
 	updatePNID(testData);
 	await sleep(1000);
+	var testData = [{"name": "ox_top_tank_pressure", "value": 32.0}];
+	updatePNID(testData);
+	await sleep(1000);
+	var testData = [{"name": "ox_bottom_tank_pressure", "value": 32.0}, {"name": "ox_top_tank_pressure", "value": 0.5}];
+	updatePNID(testData);
+	await sleep(1000);
+	var testData = [{"name": "ox_bottom_tank_pressure", "value": 0.0}, {"name": "chamber_pressure", "value": 40}];
+	updatePNID(testData);
+	await sleep(1000);
 	var testData = [{"name": "fuel_pressurize_solenoid", "value": 5.0}, {"name": "fuel_depressurize_solenoid", "value": 1.0}];
 	updatePNID(testData);
 	await sleep(1000);
@@ -79,18 +90,38 @@ function setState(state)
 	elementGroup.find("text.value").text(state["value"]);
 	console.log("Found following elements to update:", $(document).find("g." + state["name"]));
 	
+	//----- prepare for eval behavior block
 	//In Variables for the eval() code specified in config.json. Will be reset/overwritten for every state and every loop
-	let inVars = {
+	const inVars = {
 		"value" : state["value"]
 	};
 	
-	//State storage for the eval() code specified in config.json //TBD (let eval code create entries? pre-define generic name entries?)
+	//State storage for the eval() code specified in config.json //TBD (let eval code create entries? pre-define generic name entries? are they even persistent between loops right now?)
 	var stateVars = { };
 	
 	//Return values from eval() code specified in config.json. Will be applied to PnID and cleared for every state and every loop
 	let outVars = { };
 	
-	//traverse JSON to find all evals applicable to current element. evals later in JSON overwrite changes made by evals earlier (if they change the same things)
+	//----- search applicable eval behavior blocks from config files (either default config or custom config)
+	//fetch all classes of the element group into an array
+	let classes = elementGroup.attr("class").split(" ");
+	//check if applicable eval (to current element) exists in default JSON
+	for (classIndex in classes) //search through attributes to find class attribute related to type (eg: PnID-Valve_Manual)
+	{
+		console.log("Checking attribute", classIndex, classes);
+		if ("wire" in classes)
+		{
+			typeClass = "PnID-Sensor_Pressure";
+		}
+		let typeClass = classes[classIndex];
+		let re = /PnID-\S*/;
+		if (re.test(typeClass) && (typeClass in defaultConfig))
+		{
+			eval(defaultConfig[typeClass]["eval"]);
+		}
+	}
+
+	//traverse custom JSON to find all evals applicable to current element. evals later in JSON overwrite changes made by evals earlier (if they change the same parameters)
 	let configProperties = Object.keys(config);
 	for (propIndex in configProperties)
 	{
@@ -102,6 +133,21 @@ function setState(state)
 		}
 	}
 	
+	//----- apply results of eval behavior blocks to element
+	/*if ("PnID-Sensor_Pressure" in classes) //if the element currently being updated is a pressure sensor, update its corresponding wire group
+	{
+		console.log("Need to update wire group:", state["name"]);
+		updatePNID([{"name": state["name"] + "_wire", "value": state["value"]}])
+	}
+	else
+	{
+		
+	}*/
+	applyUpdatesToPnID(elementGroup, outVars);
+}
+
+function applyUpdatesToPnID(elementGroup, outVars)
+{
 	//fetch all attributes of the element group
 	let attributes = elementGroup.prop("attributes");
 	//console.log("Found these attributes:", attributes);
@@ -116,7 +162,7 @@ function setState(state)
 				break;
 			}
 			let attribute = attributes[attrIndex];
-			let re = /data-pnid-.*/;
+			let re = /data-pnid-\S*/;
 			if (re.test(attribute.name))
 			{
 				elementGroup.attr(attribute.name, outVars["color"]);
@@ -126,6 +172,10 @@ function setState(state)
 	if ("value" in outVars)
 	{
 		elementGroup.find("text.value").text(outVars["value"]);
+	}
+	if ("wireUpdate" in outVars)
+	{
+		updatePnID(outVars["wireUpdate"]);
 	}
 }
 
