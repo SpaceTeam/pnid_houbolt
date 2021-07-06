57b348f558537635160a5effc16faeaa17ccd828
LuPro <luis.immanuel@gmx.at>
2021-07-06 04:03:11 +0200

    integration for mouseevent click support for popups

---
+++ client/js/popupManager.js
@@ -0,0 +1,15 @@
+function clickEventListenever(dispReference, dispType, dispValReference) {
+	console.log(dispReference, dispValReference);
+}
+
+/*$(document).find("svg").click(function(event) {
+	console.log($(this));
+});
+
+window.addEventListener('click', (event) => {
+	console.log(event.target);
+});
+
+window.addEventListenever('hover', (event) => {
+	
+});*/
