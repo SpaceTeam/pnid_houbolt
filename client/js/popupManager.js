1160c07117597222abe8be401a492b5bf1aed834
ReneZwiletitsch <rene.zwiletitsch@gmail.com>
2021-07-09 14:25:26 +0200

    Added Feature 'X button to close popup'

--- client/js/popupManager.js
+++ client/js/popupManager.js
@@ -5,11 +5,7 @@ function clickEventListenever(dispReference, dispType, dispValReference)
 {
 	console.log(dispReference, dispValReference);
 	let popupName = dispType + "_" + dispValReference;
-	if (popupName in activePopups) //check if popup already exists
-	{
-		destroyPopup(popupName)
-	}
-	else
+	if (!(popupName in activePopups))
 	{
 		showPopup(dispReference, dispType, dispValReference);
 	}
@@ -39,23 +35,33 @@ function createPopup(parent, type, name, contentList)
 	console.log(parent);
 	console.log("parentpos:", parentPosition, "top:", parentPosition.top, "left", parentPosition.left);
 	console.log("parent width:", parent[0].getBoundingClientRect().width);
+
 	var popup = $(`<div style='position: absolute; top: ` + parentPosition.top + `px; left: ` +
-	(parentPosition.left + parent[0].getBoundingClientRect().width / 2.0) + `px; display: none;' class="popup"><div class="heading">` +
+	(parentPosition.left + parent[0].getBoundingClientRect().width / 2.0) + `px; display: none;' class="popup"><button class="popup-close" id="fill" onclick="destroyPopup($(this))">X</button><div class="heading">` +
 	name + `</div></div>`);
 	$(document.body).append(popup);
 	popup.fadeIn(50);
-	
+
+	//replace the id with the one it should have
+	var newid = type + "_" + name;  
+	console.log(newid)
+	$(".popup-close").each(function(i){
+		if(this.id =="fill"){
+			this.id = newid;
+		}
+	});
 	activePopups[type + "_" + name] = popup;
 }
 
 function destroyPopup(popupName)
 {
+	let id = popupName.attr('id');	
 	console.log(activePopups[popupName]);
-	console.log("popup:", activePopups[popupName], "filtered for visible:", activePopups[popupName].filter(":visible"));
-	//activePopups[popupName].fadeOut(2000/*, activePopups[popupName].remove()*/);
+	//console.log("popup:", activePopups[popupName], "filtered for visible:", activePopups[popupName].filter(":visible"));
+	//activePopups[popupName].fadeOut(2000/*, activePopups[popupName].remove()); 
 	//activePopups[popupName].fadeOut(2000, console.log("fade out complete"));
-	activePopups[popupName].remove();
-	delete activePopups[popupName];
+	activePopups[id].remove();
+	delete activePopups[id];
 }
 
 function pinPopup()
