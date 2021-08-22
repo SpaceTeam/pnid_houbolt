fda4f2bef5a481d4b1b77dbd04c598fc83fd8926
ReneZwiletitsch <rene.zwiletitsch@gmail.com>
2021-08-23 00:37:52 +0200

    added untracked filed to last commit

---
+++ client/js/darkmode.js
@@ -0,0 +1,17 @@
+function darkModeToggleButton(text) {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    
+    
+    if (theme.getAttribute('href') == "css/lightpnid.css") {
+        theme.setAttribute('href', "css/darkpnid.css");
+
+    } else {
+        theme.setAttribute('href', "css/lightpnid.css");
+    }
+
+    
+    console.log(text);
+}
+
+ 
