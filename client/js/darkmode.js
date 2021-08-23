96eb92fafbe4c8f4c654a4896c57055f07c15b04
ReneZwiletitsch <rene.zwiletitsch@gmail.com>
2021-08-23 18:17:54 +0200

    minor code cleanup

--- client/js/darkmode.js
+++ client/js/darkmode.js
@@ -1,17 +1,12 @@
 function darkModeToggleButton(text) {
     var element = document.body;
     element.classList.toggle("dark-mode");
-    
-    
     if (theme.getAttribute('href') == "css/lightpnid.css") {
         theme.setAttribute('href', "css/darkpnid.css");
 
     } else {
         theme.setAttribute('href', "css/lightpnid.css");
     }
-
-    
-    console.log(text);
 }
 
  
