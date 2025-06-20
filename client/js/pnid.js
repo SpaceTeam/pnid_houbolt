3c73015266c6c35e96d18edfd045e8a9fb9bd4a3
Luis Büchi <luis.immanuel@gmx.at>
2025-06-21 01:06:40 +0200

    init pnid popup hitboxes for elements with action references

--- client/js/pnid.js
+++ client/js/pnid.js
@@ -201,7 +201,9 @@ function initPNIDHitboxes()
         //only create bounding box rectangle if there is a popup definition for it - otherwise it doesn't need the hitbox
         //the .replace("_Slim", "") is a dirty hack to get the other variant of tanks to use the same config as the main type
         if (getConfigData(defaultConfig, getTypeFromClasses(pnidComps.eq(index).attr("class").split(" ")).replace("_Slim", "").replace("_Short", ""), "popup") != undefined ||
-            getConfigData(config, getValReferenceFromClasses(pnidComps.eq(index).attr("class").split(" ")).replace("-", ":"), "popup") != undefined
+            getConfigData(config, getValReferenceFromClasses(pnidComps.eq(index).attr("class").split(" ")).replace("-", ":"), "popup") != undefined ||
+            getConfigData(defaultConfig, pnidComps.eq(index).data("action-reference").replace("-", ":"), "popup") != undefined ||
+            getConfigData(config, pnidComps.eq(index).data("action-reference").replace("-", ":"), "popup") != undefined
         ) {
             let elementGraphics = pnidComps.eq(index).find("g")[0];
 
