c958dfa416d07b30dd135056f9ce5ca105d8fe18
spaceteamofficial <github@spaceteam.at>
2025-10-09 23:46:22 +0200

    pfuigacks

--- client/js/hacks.js
+++ client/js/hacks.js
@@ -3,25 +3,3 @@
  * but we need them anyways for some reason
  */
 
-function startHeartbeatPoll()
-{
-    setInterval(pollHeartbeatState, 5000);
-}
-
-function pollHeartbeatState()
-{
-    stateUpdate("engine_ecu:GetGSEConnectionAbortEnable", 0);
-    stateUpdate("engine_ecu:GetGSEConnectionAbortTimer", 0);
-    stateUpdate("fuel_ecu:GetGSEConnectionAbortEnable", 0);
-    stateUpdate("fuel_ecu:GetGSEConnectionAbortTimer", 0);
-    stateUpdate("ox_ecu:GetGSEConnectionAbortEnable", 0);
-    stateUpdate("ox_ecu:GetGSEConnectionAbortTimer", 0);
-}
-
-function setHeartbeatActive(active)
-{
-    let value = active ? 1 : 0;
-    stateUpdate("engine_ecu:SetGSEConnectionAbortEnable", value);
-    stateUpdate("fuel_ecu:SetGSEConnectionAbortEnable", value);
-    stateUpdate("ox_ecu:SetGSEConnectionAbortEnable", value);
-}
